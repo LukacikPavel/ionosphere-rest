af21063aeaa06e654501fafb5200940c58251950
LukacikPavel <p.lukacik11@gmail.com>
2021-06-13 15:13:23 +0200

    added cors configuration

--- src/main/java/sk/upjs/ics/ionosphererest/IonosphereRestApplication.java
+++ src/main/java/sk/upjs/ics/ionosphererest/IonosphereRestApplication.java
@@ -2,6 +2,9 @@ package sk.upjs.ics.ionosphererest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class IonosphereRestApplication {
@@ -10,4 +13,13 @@ public class IonosphereRestApplication {
 		SpringApplication.run(IonosphereRestApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
