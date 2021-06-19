e8bc5777114f1cabdc78b00c032faf179d90e7b3
LukacikPavel <p.lukacik11@gmail.com>
2021-06-19 22:18:43 +0200

    modified getScatterData

--- src/main/java/sk/upjs/ics/ionosphererest/controller/IonosphereController.java
+++ src/main/java/sk/upjs/ics/ionosphererest/controller/IonosphereController.java
@@ -12,8 +12,6 @@ import sk.upjs.ics.ionosphererest.model.*;
 import sk.upjs.ics.ionosphererest.repository.IonosphereRepository;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,10 +26,10 @@ public class IonosphereController {
 
     @GetMapping("/heatmap")
     public ResponseEntity<List<Data>>
-    getHeatmapData2(@RequestParam() String attribute,
-                        @RequestParam() String station,
-                        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant timeStart,
-                        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant timeEnd) {
+    getHeatmapData(@RequestParam() String attribute,
+                   @RequestParam() String station,
+                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant timeStart,
+                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant timeEnd) {
         try {
             List<Ionosphere> ionospheres = ionosphereRepository
                     .findByStationAndTimeStartGreaterThanEqualAndTimeEndLessThanEqual(station, timeStart, timeEnd);
@@ -39,29 +37,29 @@ public class IonosphereController {
             Map<AzimuthElevation, Data> map = new HashMap<>();
             Map<AzimuthElevation, Integer> map2 = new HashMap<>();
             Data heatmap;
-            for(Ionosphere elem: ionospheres){
+            for (Ionosphere elem : ionospheres) {
                 AzimuthElevation azimuthElevation = new AzimuthElevation(elem);
-                if (map.containsKey(azimuthElevation)){
+                if (map.containsKey(azimuthElevation)) {
                     heatmap = map.get(azimuthElevation);
 
-                    if(attribute.equals("tecu")){
-                        ((HeatmapTecu)heatmap).setTecu(((HeatmapTecu)heatmap).getTecu() + elem.getTecu());
-                    } else if(attribute.equals("s4")){
-                        ((HeatmapS4)heatmap).setS4(((HeatmapS4)heatmap).getS4() + elem.getS4());
-                    } else if (attribute.equals("sigmaphi")){
-                        ((HeatmapSigmaphi)heatmap).setSigmaphi(((HeatmapSigmaphi)heatmap).getSigmaphi() + elem.getSigmaphi());
+                    if (attribute.equals("tecu")) {
+                        ((HeatmapTecu) heatmap).setTecu(((HeatmapTecu) heatmap).getTecu() + elem.getTecu());
+                    } else if (attribute.equals("s4")) {
+                        ((HeatmapS4) heatmap).setS4(((HeatmapS4) heatmap).getS4() + elem.getS4());
+                    } else if (attribute.equals("sigmaphi")) {
+                        ((HeatmapSigmaphi) heatmap).setSigmaphi(((HeatmapSigmaphi) heatmap).getSigmaphi() + elem.getSigmaphi());
                     } else {
                         throw new Exception();
                     }
 
                     map.put(azimuthElevation, heatmap);
-                    map2.put(azimuthElevation, map2.get(azimuthElevation)+1);
+                    map2.put(azimuthElevation, map2.get(azimuthElevation) + 1);
                 } else {
-                    if(attribute.equals("tecu")){
+                    if (attribute.equals("tecu")) {
                         heatmap = new HeatmapTecu(elem);
-                    } else if(attribute.equals("s4")){
+                    } else if (attribute.equals("s4")) {
                         heatmap = new HeatmapS4(elem);
-                    } else if (attribute.equals("sigmaphi")){
+                    } else if (attribute.equals("sigmaphi")) {
                         heatmap = new HeatmapSigmaphi(elem);
                     } else {
                         throw new Exception();
@@ -79,12 +77,12 @@ public class IonosphereController {
                     .stream()
                     .map(elem -> {
                         int count = map2.get(new AzimuthElevation(elem));
-                        if(attribute.equals("tecu")){
-                            ((HeatmapTecu)elem).setTecu((double)Math.round((((HeatmapTecu)elem).getTecu()/count)*1000)/1000);
-                        } else if(attribute.equals("s4")){
-                            ((HeatmapS4)elem).setS4((double)Math.round((((HeatmapS4)elem).getS4()/count)*1000)/1000);
-                        } else if (attribute.equals("sigmaphi")){
-                            ((HeatmapSigmaphi)elem).setSigmaphi((double)Math.round((((HeatmapSigmaphi)elem).getSigmaphi()/count)*1000)/1000);
+                        if (attribute.equals("tecu")) {
+                            ((HeatmapTecu) elem).setTecu((double) Math.round((((HeatmapTecu) elem).getTecu() / count) * 1000) / 1000);
+                        } else if (attribute.equals("s4")) {
+                            ((HeatmapS4) elem).setS4((double) Math.round((((HeatmapS4) elem).getS4() / count) * 1000) / 1000);
+                        } else if (attribute.equals("sigmaphi")) {
+                            ((HeatmapSigmaphi) elem).setSigmaphi((double) Math.round((((HeatmapSigmaphi) elem).getSigmaphi() / count) * 1000) / 1000);
                         }
                         return elem;
                     })
@@ -98,50 +96,61 @@ public class IonosphereController {
 
     @GetMapping("/scatter")
     public ResponseEntity<List<ScatterData>>
-    getScatterData2(@RequestParam() String attribute,
+    getScatterData(@RequestParam String attribute,
                    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant timeStart,
-                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant timeEnd){
+                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant timeEnd,
+                   @RequestParam(required = false, defaultValue = "0") int azimuthStart,
+                   @RequestParam(required = false, defaultValue = "360") int azimuthEnd,
+                   @RequestParam(required = false, defaultValue = "0") int elevationStart,
+                   @RequestParam(required = false, defaultValue = "90") int elevationEnd) {
         try {
             List<Ionosphere> ionospheres =
-                    ionosphereRepository.findByTimeStartGreaterThanEqualAndTimeEndLessThanEqual(timeStart, timeEnd);
+                    ionosphereRepository.findByTimeStartGreaterThanEqualAndTimeEndLessThanEqualAndAzimuthStartGreaterThanEqualAndAzimuthEndLessThanEqualAndElevationStartGreaterThanEqualAndElevationEndLessThanEqual
+                            (timeStart, timeEnd, azimuthStart, azimuthEnd, elevationStart, elevationEnd);
 
             Map<StationTimeStartEnd, ScatterData> map = new HashMap<>();
             Map<StationTimeStartEnd, Integer> map2 = new HashMap<>();
             ScatterData data;
-            for(Ionosphere elem: ionospheres){
+            for (Ionosphere elem : ionospheres) {
                 StationTimeStartEnd stationTimeStartEnd = new StationTimeStartEnd(elem);
                 boolean scintillation = isScintillation(elem);
 
-                if (map.containsKey(stationTimeStartEnd)){
+                if (map.containsKey(stationTimeStartEnd)) {
                     data = map.get(stationTimeStartEnd);
 
-                    if(attribute.equals("tecu")){
-                        ((ScatterTecu)data).setTecu(((ScatterTecu)data).getTecu() + elem.getTecu());
-                    } else if(attribute.equals("s4")){
-                        ((ScatterS4)data).setS4(((ScatterS4)data).getS4() + elem.getS4());
-                    } else if (attribute.equals("sigmaphi")){
-                        ((ScatterSigmaphi)data).setSigmaphi(((ScatterSigmaphi)data).getSigmaphi() + elem.getSigmaphi());
+                    if (attribute.equals("tecu")) {
+                        ((ScatterTecu) data).setTecu(((ScatterTecu) data).getTecu() + elem.getTecu());
+                    } else if (attribute.equals("s4")) {
+                        ((ScatterS4) data).setS4(((ScatterS4) data).getS4() + elem.getS4());
+                    } else if (attribute.equals("sigmaphi")) {
+                        ((ScatterSigmaphi) data).setSigmaphi(((ScatterSigmaphi) data).getSigmaphi() + elem.getSigmaphi());
                     } else {
                         throw new Exception();
                     }
 
-                    if (scintillation){
+                    if (scintillation) {
                         data.setScintillation(true);
                     }
+
+                    data.setAzimuthStart(azimuthStart);
+                    data.setAzimuthEnd(azimuthEnd);
+                    data.setElevationStart(elevationStart);
+                    data.setElevationEnd(elevationEnd);
+
                     map.put(stationTimeStartEnd, data);
-                    map2.put(stationTimeStartEnd, map2.get(stationTimeStartEnd)+1);
+                    map2.put(stationTimeStartEnd, map2.get(stationTimeStartEnd) + 1);
                 } else {
-                    if(attribute.equals("tecu")){
+                    if (attribute.equals("tecu")) {
                         data = new ScatterTecu(elem);
-                    } else if(attribute.equals("s4")){
+                    } else if (attribute.equals("s4")) {
                         data = new ScatterS4(elem);
-                    } else if (attribute.equals("sigmaphi")){
+                    } else if (attribute.equals("sigmaphi")) {
                         data = new ScatterSigmaphi(elem);
                     } else {
                         throw new Exception();
                     }
 
-                    if (scintillation){
+                    if (scintillation) {
                         data.setScintillation(true);
                     }
                     map.put(stationTimeStartEnd, data);
@@ -154,12 +163,12 @@ public class IonosphereController {
                     .stream()
                     .map(elem -> {
                         int count = map2.get(new StationTimeStartEnd(elem));
-                        if(attribute.equals("tecu")){
-                            ((ScatterTecu)elem).setTecu((double)Math.round((((ScatterTecu)elem).getTecu()/count)*1000)/1000);
-                        } else if(attribute.equals("s4")){
-                            ((ScatterS4)elem).setS4((double)Math.round((((ScatterS4)elem).getS4()/count)*1000)/1000);
-                        } else if (attribute.equals("sigmaphi")){
-                            ((ScatterSigmaphi)elem).setSigmaphi((double)Math.round((((ScatterSigmaphi)elem).getSigmaphi()/count)*1000)/1000);
+                        if (attribute.equals("tecu")) {
+                            ((ScatterTecu) elem).setTecu((double) Math.round((((ScatterTecu) elem).getTecu() / count) * 1000) / 1000);
+                        } else if (attribute.equals("s4")) {
+                            ((ScatterS4) elem).setS4((double) Math.round((((ScatterS4) elem).getS4() / count) * 1000) / 1000);
+                        } else if (attribute.equals("sigmaphi")) {
+                            ((ScatterSigmaphi) elem).setSigmaphi((double) Math.round((((ScatterSigmaphi) elem).getSigmaphi() / count) * 1000) / 1000);
                         }
                         return elem;
                     })
@@ -172,24 +181,13 @@ public class IonosphereController {
         }
     }
 
-    private boolean isScintillation(Ionosphere ionosphere){
+    private boolean isScintillation(Ionosphere ionosphere) {
         if (ionosphere.getElevationStart() >= 30 &&
-                (ionosphere.getS4() > 0.6 || ionosphere.getSigmaphi() > 0.1)){
+                (ionosphere.getS4() > 0.6 || ionosphere.getSigmaphi() > 0.1)) {
             return true;
         }
 
         return false;
-    }
-
-    @GetMapping("/count")
-    public ResponseEntity<Integer> findByTimeStart() {
-        try {
-            Integer count = ionosphereRepository.getCount();
-            return new ResponseEntity<>(count, HttpStatus.OK);
-        } catch (Exception e) {
-            System.out.println(e);
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
     @GetMapping("/stations")
--- src/main/java/sk/upjs/ics/ionosphererest/repository/IonosphereRepository.java
+++ src/main/java/sk/upjs/ics/ionosphererest/repository/IonosphereRepository.java
@@ -8,27 +8,18 @@ import sk.upjs.ics.ionosphererest.model.Ionosphere;
 import sk.upjs.ics.ionosphererest.model.Station;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IonosphereRepository extends CassandraRepository<Ionosphere, MapId> {
 
-//    @AllowFiltering
-//    <T> List<T> findByStationAndTimeStartGreaterThanEqualAndTimeEndLessThanEqual(Class<T> type, String station, LocalDateTime time_start, LocalDateTime time_end);
-
     @AllowFiltering
     List<Ionosphere> findByStationAndTimeStartGreaterThanEqualAndTimeEndLessThanEqual(String station, Instant time_start, Instant time_end);
 
-
-//    @AllowFiltering
-//    <T> List<T> findByTimeStartGreaterThanEqualAndTimeEndLessThanEqual(Class<T> type, LocalDateTime time_start, LocalDateTime time_end);
-
     @AllowFiltering
-    List<Ionosphere> findByTimeStartGreaterThanEqualAndTimeEndLessThanEqual(Instant time_start, Instant time_end);
+    List<Ionosphere> findByTimeStartGreaterThanEqualAndTimeEndLessThanEqualAndAzimuthStartGreaterThanEqualAndAzimuthEndLessThanEqualAndElevationStartGreaterThanEqualAndElevationEndLessThanEqual
+            (Instant timeStart, Instant timeEnd, int azimuthStart, int azimuthEnd, int elevationStart, int elevationEnd);
 
-    @Query(allowFiltering = true, value = "SELECT DISTINCT station FROM ionosphere;")
+    @Query(value = "SELECT DISTINCT station FROM ionosphere;")
     List<Station> getDistinctStations();
 
-    @Query(allowFiltering = true, value = "SELECT COUNT(*) FROM ionosphere;")
-    Integer getCount();
 }
